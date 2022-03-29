e5eef7a5e2721f89f4c4a6441edf8de643d13f83
francknumbi <17n408@esisalama.org>
2022-03-29 18:06:41 +0200

    ajout du fichier jasmin.jar

--- src/beatrice/codeGenerateur/CodeGenerateur.java
+++ src/beatrice/codeGenerateur/CodeGenerateur.java
@@ -1,7 +1,14 @@
 package beatrice.codeGenerateur;
 
 import beatrice.analysis.DepthFirstAdapter;
+import beatrice.node.AAlgorithmeProgramme;
+import jas.CodeAttr;
 
 public class CodeGenerateur extends DepthFirstAdapter {
-    
+
+    public void inAAlgorithmeProgramme(AAlgorithmeProgramme node)
+    {
+        CodeAttr init = new CodeAttr();
+        CodeAttr main_code = new CodeAttr();
+    }
 }
